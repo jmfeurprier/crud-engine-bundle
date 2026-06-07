8a5c3889328c76c52fbecfed9721ebe9128e061b
JM Feurprier <jmfeurprier@gmail.com>
2026-06-07 14:04:35 -0400

    Remove stray empty use statements

--- src/Compilation/Resolution/ConfigurationValueResolver.php
+++ src/Compilation/Resolution/ConfigurationValueResolver.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Jmf\CrudEngine\Compilation\Resolution;
 
-use;
 use Jmf\CrudEngine\Exception\CrudEngineInvalidConfigurationException;
 
 readonly class ConfigurationValueResolver
--- src/Compilation/Resolution/OverridableConfigurationValueResolver.php
+++ src/Compilation/Resolution/OverridableConfigurationValueResolver.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Jmf\CrudEngine\Compilation\Resolution;
 
-use;
 use Jmf\CrudEngine\Exception\CrudEngineInvalidConfigurationException;
 use Webmozart\Assert\Assert;
 
